61faf14ccff41ea64b5179471c750c0db38d6764
zxk901021 <447360581@qq.com>
2017-04-11 15:07:41 +0800

    FIRST COMMIT

---
+++ src/main/java/com/zhy_9/productmanager/ContractListActivity.java
@@ -0,0 +1,13 @@
+package com.zhy_9.productmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContractListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contract_list);
+    }
+}
---
+++ src/main/java/com/zhy_9/productmanager/MainActivity.java
@@ -0,0 +1,28 @@
+package com.zhy_9.productmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_submit)Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login_submit)
+    public void submit(){
+        Intent intent = new Intent(MainActivity.this, ContractActivity.class);
+        startActivity(intent);
+    }
+}
